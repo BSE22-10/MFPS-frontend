36cd0a2c32154f58fc00923db93295f7157159c7
Odeke12 <trevodex@gmail.com>
2022-07-21 00:03:21 +0300

    Can view daily activity

--- src/sections/@dashboard/app/DashChart.js
+++ src/sections/@dashboard/app/DashChart.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
 import merge from 'lodash/merge';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import BeatLoader from 'react-spinners/BeatLoader';
 // import ReactApexChart from 'react-apexcharts';
 import Chart from 'react-apexcharts';
 // @mui
@@ -11,23 +12,10 @@ import { BaseOptionChart } from '../../../components/chart';
 
 // ----------------------------------------------------------------------
 
-const stuff = {
-  options: {
-    chart: {
-      id: 'basic-bar',
-    },
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-    },
-  },
-  series: [
-    {
-      name: 'series-1',
-      data: [30, 40, 45, 50, 49, 60, 70, 91],
-    },
-  ],
-};
-
+var categories = [];
+var series = [];
+var chartOptions;
+var chartSeries;
 DashChart.propTypes = {
   title: PropTypes.string,
   // subheader: PropTypes.string,
@@ -35,22 +23,125 @@ DashChart.propTypes = {
   // chartLabels: PropTypes.arrayOf(PropTypes.string).isRequired,
   chartinfo: PropTypes.object,
   charttype: PropTypes.string,
+  // timely: PropTypes.bool,
 };
 
-export default function DashChart({ title, subheader, chartLabels, chartData, chartinfo, charttype, ...other }) {
+export default function DashChart({
+  title,
+  subheader,
+  chartLabels,
+  chartData,
+  chartinfo,
+  charttype,
+  timely,
+  ...other
+}) {
+  chartOptions = chartinfo.options;
+  chartSeries = chartinfo.series;
   console.log(chartinfo);
-  const [categories, setCategories] = useState();
+  // const [categories, setCategories] = useState();
+  const [timelyData, setTimelyDate] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const isMounted = useRef(false);
+  console.log(timely);
+  if (timely === 'true') {
+    console.log('Here we are');
+    useEffect(() => {
+      setLoading(true);
+      axios({
+        method: 'get',
+        url: `${process.env.REACT_APP_API_URL}/slots/timelyData`,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((result) => {
+          setLoading(false);
+          console.log(result);
+          result.data.map((info) => {
+            categories.push(info.time);
+            //   setCategory((category) => [...category, info.floor_id]);
+            // setSerie((serie) => [...serie, info.count]);
+            // setCategory(info.categories);
+            // setCategory((category) => {
+            //   category.push(info.floor_id);
+            // });
+            series.push(info.count);
+            return true;
+          });
+          const stuff = {
+            options: {
+              chart: {
+                id: 'basic-bar',
+              },
+              xaxis: {
+                categories: categories,
+              },
+              yaxis: [
+                {
+                  axisTicks: {
+                    show: true,
+                  },
+                  axisBorder: {
+                    show: true,
+                    color: 'inherit',
+                  },
+                  // labels: {
+                  //   style: {
+                  //     colors: '#FF1654',
+                  //   },
+                  // },
+                  title: {
+                    text: 'Number of cars',
+                    // style: {
+                    //   color: '#FF1654',
+                    // },
+                  },
+                },
+              ],
+            },
+            series: [
+              {
+                name: 'Daily activity',
+                data: series,
+              },
+            ],
+          };
+          setData(stuff);
+        })
+        .catch((error) => {
+          console.log(error);
+          // setError(error.response.data);
+        });
+    }, []);
+    chartOptions = data.options;
+    chartSeries = data.series;
+  }
+  console.log(data);
   // setCategories(chartinfo.options.xaxis.categories.categories);
   // console.log(other);
-
   return (
-    <Card {...other}>
-      <CardHeader title={title} subheader={subheader} />
+    <div>
+      {loading === true && timely === 'true' ? (
+        <div className="loading">
+          <BeatLoader
+            color="#542A52"
+            loading={loading}
+            // css={override}
+            size={40}
+          />
+        </div>
+      ) : (
+        <Card {...other}>
+          <CardHeader title={title} subheader={subheader} />
 
-      <Box sx={{ p: 3, pb: 1 }} dir="ltr">
-        {/* <ReactApexChart type="line" series={chartData} options={chartOptions} height={364} /> */}
-        <Chart options={chartinfo.options} series={chartinfo.series} type={charttype} width="100%" height="364" />
-      </Box>
-    </Card>
+          <Box sx={{ p: 3, pb: 1 }} dir="ltr">
+            {/* <ReactApexChart type="line" series={chartData} options={chartOptions} height={364} /> */}
+            <Chart options={chartOptions} series={chartSeries} type={charttype} width="100%" height="364" />
+          </Box>
+        </Card>
+      )}
+    </div>
   );
 }
