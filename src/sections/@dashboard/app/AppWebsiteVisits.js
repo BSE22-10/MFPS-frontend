14aade08d949d4149d6361dcc0400c4d6849ca9e
Odeke12 <trevodex@gmail.com>
2022-07-20 13:35:40 +0300

    Updated the bar chart

--- src/sections/@dashboard/app/AppWebsiteVisits.js
+++ src/sections/@dashboard/app/AppWebsiteVisits.js
@@ -1,12 +1,78 @@
 import PropTypes from 'prop-types';
 import merge from 'lodash/merge';
-import ReactApexChart from 'react-apexcharts';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+// import ReactApexChart from 'react-apexcharts';
+import Chart from 'react-apexcharts';
 // @mui
 import { Card, CardHeader, Box } from '@mui/material';
 // components
 import { BaseOptionChart } from '../../../components/chart';
 
 // ----------------------------------------------------------------------
+const [floorData, setData] = useState([]);
+const [loading, setLoading] = useState();
+useEffect(() => {
+  setLoading(true);
+  axios({
+    method: 'get',
+    url: `${process.env.REACT_APP_API_URL}/slots/parkingSlots`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((result) => {
+      setLoading(false);
+      const categories = [];
+      const series = [];
+      console.log(result);
+      result.data.map((info) => {
+        console.log(info);
+        categories.push(info.floor_id);
+        series.push(info.count);
+        return true;
+      });
+      console.log(categories);
+      const stuff = {
+        options: {
+          chart: {
+            id: 'basic-bar',
+          },
+          xaxis: {
+            categories: { categories },
+          },
+        },
+        series: [
+          {
+            name: 'series-1',
+            data: { series },
+          },
+        ],
+      };
+      setData(stuff);
+    })
+    .catch((error) => {
+      console.log(error);
+      // setError(error.response.data);
+    });
+}, []);
+
+const stuff = {
+  options: {
+    chart: {
+      id: 'basic-bar',
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+    },
+  },
+  series: [
+    {
+      name: 'series-1',
+      data: [30, 40, 45, 50, 49, 60, 70, 91],
+    },
+  ],
+};
 
 AppWebsiteVisits.propTypes = {
   title: PropTypes.string,
@@ -16,6 +82,7 @@ AppWebsiteVisits.propTypes = {
 };
 
 export default function AppWebsiteVisits({ title, subheader, chartLabels, chartData, ...other }) {
+  // console.log(other);
   const chartOptions = merge(BaseOptionChart(), {
     plotOptions: { bar: { columnWidth: '16%' } },
     fill: { type: chartData.map((i) => i.fill) },
@@ -40,7 +107,8 @@ export default function AppWebsiteVisits({ title, subheader, chartLabels, chartD
       <CardHeader title={title} subheader={subheader} />
 
       <Box sx={{ p: 3, pb: 1 }} dir="ltr">
-        <ReactApexChart type="line" series={chartData} options={chartOptions} height={364} />
+        {/* <ReactApexChart type="line" series={chartData} options={chartOptions} height={364} /> */}
+        <Chart options={floorData.options} series={floorData.series} type="bar" width="100%" height="364" />
       </Box>
     </Card>
   );
