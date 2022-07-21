1c380fc92d1bba1245069d32e86e30771a9e3538
Odeke12 <trevodex@gmail.com>
2022-07-21 11:28:23 +0300

    Updated routes

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,12 @@ import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
 //
 import Blog from './pages/Blog';
-import User from './pages/User';
 import Login from './pages/Login';
 import NotFound from './pages/Page404';
 import Register from './pages/Register';
 import Products from './pages/Products';
 import DashboardApp from './pages/DashboardApp';
+import Transactions from './pages/Transactions';
 
 // ----------------------------------------------------------------------
 
@@ -20,7 +20,7 @@ export default function Router() {
       element: <DashboardLayout />,
       children: [
         { path: 'app', element: <DashboardApp /> },
-        { path: 'user', element: <User /> },
+        { path: 'user', element: <Transactions /> },
         { path: 'products', element: <Products /> },
         { path: 'blog', element: <Blog /> },
       ],
