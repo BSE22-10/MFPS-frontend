09329710f332de0499bcfcedfa872a97de0e0123
Odeke12 <trevodex@gmail.com>
2022-07-21 16:35:58 +0300

    Updated profile picture

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -3,7 +3,7 @@
 const account = {
   displayName: 'Admin',
   email: 'demo@minimals.cc',
-  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
+  photoURL: '/static/mock-images/avatars/avatar_23.jpg',
 };
 
 export default account;
