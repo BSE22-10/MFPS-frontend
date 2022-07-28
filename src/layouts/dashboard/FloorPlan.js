f60daff6206b9535a83a9180f91a7487201adffd
Odeke12 <trevodex@gmail.com>
2022-07-28 19:12:13 +0300

    Updated FloorPlan

--- src/layouts/dashboard/FloorPlan.js
+++ src/layouts/dashboard/FloorPlan.js
@@ -5,13 +5,19 @@ import { FloorContext } from 'src/pages/DashboardApp';
 import SlotCard from './SlotCard';
 import Searchbar from './Searchbar';
 import BeatLoader from 'react-spinners/BeatLoader';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
 function FloorPlan(props) {
   const floor = useContext(FloorContext);
   const [loading, setLoading] = useState(true);
   const [slots, setSlots] = useState([]);
+  // const [occupied, setOccupuied] = useState(0);
+  // const [available, setAvailable] = useState(0);
   var count = 0;
+  var occupied = 0,
+    available = 0;
   useEffect(() => {
     setLoading(true);
     axios({
@@ -22,8 +28,8 @@ function FloorPlan(props) {
       },
     })
       .then((result) => {
-        setLoading(false);
         setSlots(result.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
@@ -53,7 +59,20 @@ function FloorPlan(props) {
         ) : (
           slots.map((slot) => {
             count += 1;
+            console.log(slot.SlotStatus[0] === undefined);
             // console.log(slot.SlotStatus[0]);
+            const status = slot.SlotStatus.length === 0 || slot.SlotStatus[0].status === false ? false : true;
+            // if (status === true) {
+            //   setAvailable((available) => available + 1);
+            // } else {
+            //   setOccupuied((occupied) => occupied + 1);
+            // }
+            if (status === true) {
+              occupied += 1;
+            } else {
+              available += 1;
+            }
+            console.log(status);
             return (
               <Grid item xs={6} sm={2} key={slot.id}>
                 <SlotCard slot={slot} count={count} />
@@ -62,6 +81,16 @@ function FloorPlan(props) {
           })
         )}
       </Grid>
+      <Stack spacing={1} alignItems="flex-end">
+        <Stack direction="row" spacing={1}>
+          <Chip label="Available" color="success" />
+          <Typography>{available}</Typography>
+        </Stack>
+        <Stack direction="row" spacing={1}>
+          <Chip label="Occupied" color="error" />
+          <Typography>{occupied}</Typography>
+        </Stack>
+      </Stack>
     </div>
   );
 }
