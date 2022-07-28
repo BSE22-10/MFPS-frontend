4cef3e6309180e089b460b7bc05ed01b3aaab834
Odeke12 <trevodex@gmail.com>
2022-07-28 18:32:12 +0300

    Added status of parking slots

--- src/layouts/dashboard/Accordian.js
+++ src/layouts/dashboard/Accordian.js
@@ -4,20 +4,24 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import LinearProgress from '@mui/material/LinearProgress';
 import { Box } from '@mui/material';
 import axios from 'axios';
+import { FloorContext } from 'src/pages/DashboardApp';
+import FloorPlan from './FloorPlan';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
 export default function Accordian(props) {
-  //   console.log(props);
+  console.log(props);
   const [expanded, setExpanded] = useState(false);
   const [loading, setLoading] = useState(true);
   const [slots, setSlots] = useState([]);
+  const floor = useContext(FloorContext);
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
-  console.log(props.id);
   useEffect(() => {
     setLoading(true);
     axios({
@@ -30,7 +34,7 @@ export default function Accordian(props) {
       .then((result) => {
         setLoading(false);
         setSlots(result.data);
-        // console.log(result.data);
+        console.log(result.data);
       })
       .catch((error) => {
         console.log(error);
@@ -39,28 +43,25 @@ export default function Accordian(props) {
 
   return (
     <div>
-      {slots.map((slot) => {
-        {
-          if (loading) {
-            <Box sx={{ width: '100%' }}>
-              <LinearProgress />
-            </Box>;
-          } else {
-            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-              <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1bh-content" id="panel1bh-header">
-                <Typography sx={{ width: '33%', flexShrink: 0 }}>{`Floor ${props.floor.id}`}</Typography>
-                <Typography sx={{ color: 'text.secondary' }}>{`${props.floor.no_of_slots} slots`}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget maximus est, id
-                  dignissim quam.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>;
-          }
-        }
-      })}
+      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1bh-content" id="panel1bh-header">
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>{`Floor ${props.floor.id}`}</Typography>
+          <Typography sx={{ color: 'text.secondary' }}>{`${props.floor.no_of_slots} slots`}</Typography>
+        </AccordionSummary>
+        <AccordionDetails sx={{ backgroundColor: '#c8c9cb' }}>
+          <Typography>| Entrance |</Typography>
+          <FloorPlan />
+
+          <Stack spacing={1} alignItems="flex-end">
+            <Stack direction="row" spacing={1}>
+              <Chip label="Available" color="success" />
+            </Stack>
+            <Stack direction="row" spacing={1}>
+              <Chip label="Occupied" color="error" />
+            </Stack>
+          </Stack>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 }
