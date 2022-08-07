088dc01c4fcda80373910a6567922b051a558741
Odeke12 <trevodex@gmail.com>
2022-08-07 21:30:16 +0300

    Updated routes

--- src/routes.js
+++ src/routes.js
@@ -34,7 +34,7 @@ export default function Router() {
       element: <Payments />,
     },
     {
-      path: '/homePayment',
+      path: '/homePayment/',
       element: <HomePayment />,
     },
     {
