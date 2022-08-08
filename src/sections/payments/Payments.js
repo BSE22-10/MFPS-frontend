e44e98b520e305e9cdd60bb2f17d35980b47d78d
Odeke12 <trevodex@gmail.com>
2022-08-08 15:15:30 +0300

    Updates account after successful payment

--- src/sections/payments/Payments.js
+++ src/sections/payments/Payments.js
@@ -1,38 +1,121 @@
 import React from 'react';
-import { FlutterWaveButton, closePaymentModal } from 'flutterwave-react-v3';
+import { useFlutterwave, FlutterWaveButton, closePaymentModal } from 'flutterwave-react-v3';
+import './registerPayments.css';
+import axios from 'axios';
+import { useContext, useState } from 'react';
 
-function Payments() {
+function Payments(props) {
+  const updateAccount = (plate, amount) => {
+    axios({
+      method: 'put',
+      url: `${process.env.REACT_APP_API_URL}/accounts/updatePayment`,
+      data: {
+        number_plate: plate,
+        amount: amount,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((data) => {
+        console.log(data);
+        if (data.status == 200) {
+          console.log('successfull update');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  var disabled = props.disabled;
+  const { numberPlate, email, amount } = props.value;
   const config = {
     public_key: 'FLWPUBK_TEST-fd554ed0ef13c68ce40d28394cba5c1f-X',
     tx_ref: Date.now(),
-    amount: 2,
+    amount: props.value.amount,
     currency: 'UGX',
     payment_options: 'card,mobilemoneyuganda,ussd',
     customer: {
-      email: 'user@gmail.com',
+      name: props.value.numberPlate,
+      email: props.value.email,
       phonenumber: '0771419370',
-      name: 'joel ugwumadu',
     },
     customizations: {
-      title: 'My store',
-      description: 'Payment for items in cart',
+      title: 'Parking',
+      description: 'Payment parking',
       logo: 'https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg',
     },
+    meta: {
+      numberPlate: props.value.numberPlate,
+    },
   };
+
+  const handleFlutterPayment = useFlutterwave(config);
+
+  function createAccount() {
+    axios({
+      method: 'post',
+      url: `${process.env.REACT_APP_API_URL}/accounts`,
+      data: {
+        number_plate: numberPlate,
+        email: email,
+        amount: amount,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((data) => {
+        console.log(data);
+        if (data.status == 200) {
+          handleFlutterPayment({
+            callback: (response) => {
+              var plate = response.customer.name.split(' ');
+              // console.log(plate.at(-1));
+              // console.log(response.customer.name.split(' '));
+              console.log(response.customer.name);
+              console.log(response.customer.name.split(' ').slice(0, -1).join(' '));
+              updateAccount(response.customer.name, response.amount);
+              closePaymentModal(); // this will close the modal programmatically
+            },
+            onClose: () => {},
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   const fwConfig = {
     ...config,
     text: 'Pay with Flutterwave!',
     callback: (response) => {
-      console.log(response);
+      console.log(response.meta);
       closePaymentModal(); // this will close the modal programmatically
     },
     onClose: () => {},
   };
   return (
     <div className="App">
-      <h1>Hello Test user</h1>
-      <FlutterWaveButton {...fwConfig} />
+      {/* <FlutterWaveButton {...fwConfig} /> */}
+      <button
+        disabled={!disabled}
+        onClick={() => {
+          console.log('Ypa');
+          // handleFlutterPayment({
+          //   callback: (response) => {
+          //     console.log(response.meta);
+          //     closePaymentModal(); // this will close the modal programmatically
+          //   },
+          //   onClose: () => {},
+          // });
+          createAccount();
+        }}
+        className="btnNext"
+      >
+        REGISTER
+      </button>
     </div>
   );
 }
